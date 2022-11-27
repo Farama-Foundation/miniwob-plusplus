3d087bd52abdd09d03dcea449644a61bc119099f
Panupong Pasupat <ppasupat@stanford.edu>
2022-11-26 19:10:58 -0800

    Added CI tests

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="miniwob_plusplus",
+    version="0.0.1",
+    python_requires=">=3.7, <3.11",
+    install_requires=[
+        "Gymnasium==0.26.3",
+        "Pillow>=9.0.0",
+        "selenium>=4.5.0",
+        "numpy>=1.18.0",
+    ],
+    extras_require={"testing": ["pytest>=7.0.0"]},
+)
