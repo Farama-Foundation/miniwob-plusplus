b9b7808bc75ca92c9972f6d7a7f031e0f0c22425
Panupong Pasupat <ppasupat@stanford.edu>
2023-02-12 19:17:31 -0800

    Fixed drag-circle task name + Use gymnasium API in tests.

--- tests/utils.py
+++ tests/utils.py
@@ -1,13 +1,13 @@
 """Test utilities."""
 from typing import Iterable
 
-import gymnasium as gym
+import gymnasium
 
 
 def get_all_registered_miniwob_envs() -> Iterable[str]:
     """Return the name of all registered MiniWoB environments."""
     envs = []
-    for env_id, env_spec in gym.registry.items():
+    for env_id, env_spec in gymnasium.registry.items():
         # TODO: Enable flightwob tasks.
         if env_spec.namespace == "miniwob" and "flight." not in env_id:
             envs.append(env_id)
