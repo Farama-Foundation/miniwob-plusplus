36e89c3f9af4648277018125bbf042ecfc7b1d7a
snow-fox <taijunjet@hotmail.com>
2023-05-15 18:04:27 +0100

    fix name

--- miniwob/__init__.py
+++ miniwob/__init__.py
@@ -7,8 +7,8 @@ __version__ = "0.0.1"
 try:
     from farama_notifications import notifications
 
-    if "gymnasium" in notifications and __version__ in notifications["gymnasium"]:
-        print(notifications["gymnasium"][__version__], file=sys.stderr)
+    if "miniwob-plusplus" in notifications and __version__ in notifications["miniwob-plusplus"]:
+        print(notifications["miniwob-plusplus"][__version__], file=sys.stderr)
 
 except Exception:  # nosec
     pass
