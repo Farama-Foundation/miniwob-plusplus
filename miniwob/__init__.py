4013a7e166951bf2b303ca748a788b30e767c643
Marek Wydmuch <marek@wydmuch.poznan.pl>
2026-08-13 13:13:46 +0200

    Bump version to 1.1.0

--- miniwob/__init__.py
+++ miniwob/__init__.py
@@ -4,12 +4,12 @@ import sys
 from miniwob.registration import register_miniwob_envs
 
 
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 
 register_miniwob_envs()
 
 try:
-    from farama_notifications import notifications
+    from farama_notificatgions import notifications
 
     if (
         "miniwob-plusplus" in notifications
